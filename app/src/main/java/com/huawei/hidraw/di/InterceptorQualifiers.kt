076fe00bcfb60256e9399aeb800bc7351713d370
oguz sahin <oguzsahin098@gmail.com>
2021-11-30 11:47:22 +0300

    token based network layer arranged.

---
+++ app/src/main/java/com/huawei/hidraw/di/InterceptorQualifiers.kt
@@ -0,0 +1,20 @@
+package com.huawei.hidraw.di
+
+import javax.inject.Qualifier
+
+/**
+ * Created by Oguz Sahin on 11/29/2021.
+ */
+
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class Header
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class HttpLogging
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class NetworkConnection
---
+++ app/src/main/java/com/huawei/hidraw/network/DrawService.kt
@@ -0,0 +1,8 @@
+package com.huawei.hidraw.network
+
+/**
+ * Created by Oguz Sahin on 11/29/2021.
+ */
+interface DrawService {
+
+}
