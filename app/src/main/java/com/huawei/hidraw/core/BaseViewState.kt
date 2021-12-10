101cec237860c8b0801f7d1c64c1c19e6f1064b2
Oğuz Şahin <oguzsahin098@gmail.com>
2021-12-10 10:34:55 +0300

    Revert "Create create draw view state, generic dialog fragment, input info dialog fragment, draw model, fix chip background selector"

--- app/src/main/java/com/huawei/hidraw/core/BaseViewState.kt
+++
@@ -1,9 +0,0 @@
-package com.huawei.hidraw.core
-
-import android.view.View
-
-open class BaseViewState {
-
-    fun getViewVisibility(isViewVisible: Boolean): Int =
-        if (isViewVisible) View.VISIBLE else View.GONE
-}
--- app/src/main/java/com/huawei/hidraw/data/model/BaseResponseModel.kt
+++
@@ -1,9 +0,0 @@
-package com.huawei.hidraw.data.model
-
-/**
- * Created by Oguz Sahin on 11/15/2021.
- */
-data class BaseResponseModel<T>(
-    val result: Boolean,
-    val data: T
-)
